14900fc536c564c00f9c9d05c8977c680fec5a56
luancardosoti <luancardosoti@gmail.com>
2024-11-02 16:48:45 -0300

    fix: errors

--- src/components/plinko/index.tsx
+++ src/components/plinko/index.tsx
@@ -1,16 +1,25 @@
 'use client'
 
+import { useState, useEffect } from 'react';
 import { PlinkoBoard } from "./plinko-board";
 import { PlinkForm } from "./plink-form";
 import { FairnessModal } from "../fairness-modal";
 
 export function Plinko() {
-  const width = Math.min(window.innerWidth * 0.95, 840)
-  const height = Math.min(window.innerHeight * 0.7, 600)
+  const [dimensions, setDimensions] = useState({ width: 840, height: 600 });
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const width = Math.min(window.innerWidth * 0.95, 840);
+    const height = Math.min(window.innerHeight * 0.7, 600);
+    setDimensions({ width, height });
+    setIsMobile(window.innerWidth <= 1024);
+  }, []);
+
   return (
     <div className="px-4">
       <div className="border rounded-2xl flex flex-col-reverse lg:flex-row overflow-hidden">
-        {window.innerWidth <= 1024 && (
+        {isMobile && (
           <div className="flex items-center justify-end py-2 px-4 sm:px-6 border-t">
             <FairnessModal />
           </div>
@@ -18,10 +27,10 @@ export function Plinko() {
         <PlinkForm />
         <div className="w-full">
           <PlinkoBoard
-            width={width} 
-            height={height}           
+            width={dimensions.width} 
+            height={dimensions.height}           
           />
-          {window.innerWidth > 1024 && <div className="flex items-center justify-end py-2 px-4 sm:px-6 border-t">
+          {!isMobile && <div className="flex items-center justify-end py-2 px-4 sm:px-6 border-t">
             <FairnessModal />
           </div>}
         </div>
